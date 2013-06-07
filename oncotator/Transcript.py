3142d06908ef475b3c4b4b51be3f5cc16adbe93d
Lee Lichtenstein <lichtens@broadinstitute.org>
2013-06-07 13:15:51 -0400

    Added Transcript class for encapsulating different Transcript Providers.

---
+++ oncotator/Transcript.py
@@ -0,0 +1,45 @@
+
+class Transcript(object):
+    """Simple class to hold transcript information in a standard way across Transcript Providing datasources
+
+    transcript_id -- unique id from the transcript (e.g. uc010ajp.1 for GAF) (string)
+    exons are a list of (start, end) in genomic coordinates (integers)
+
+    """
+    def __init__(self, transcript_id, gene, gene_id = "", exons = [], cds = [], seq=""):
+        self._transcript_id = transcript_id
+        self._exons = exons
+        self._cds = cds
+        self._gene = gene
+        self._gene_id = gene_id
+        self._seq = seq
+
+    def add_exon(self, start, end):
+        self._exons.append((start, end))
+
+    def add_cds(self, start, end):
+        self._cds.append((start, end))
+
+    def set_transcript_id(self, id):
+        self._transcript_id = id
+
+    def set_gene(self, gene):
+        self._transcript_id = id
+
+    def get_transcript_id(self):
+        return self._transcript_id
+
+    def get_gene(self):
+        return self._gene
+
+    def get_seq(self):
+        return self._seq
+
+    def set_seq(self, seq):
+        self._seq = seq
+
+    def get_exons(self):
+        return self._exons
+
+    def get_cds(self):
+        return self._cds
