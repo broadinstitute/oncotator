b154b0d614cb0e84a4a1c8b3a2ead1b9cbab98a3
Lee Lichtenstein <lichtens@broadinstitute.org>
2013-09-10 10:53:23 -0400

    Added dependencies to the create oncotator virtual env script.

--- scripts/create_oncotator_venv.sh
+++ scripts/create_oncotator_venv.sh
@@ -33,10 +33,37 @@ echo "PyVCF ========================="
 # This one is a little more complicated
 echo "Retrieving mgupta (aka elephanthunter) fork of PyVCF"
 wget --no-check-certificate 'https://github.com/elephanthunter/PyVCF/archive/master.zip'
-mv master master.zip && unzip master.zip && cd PyVCF-master && python setup.py install && cd .. && rm -Rf PyVCF-master && rm -f master.* && rm -f master*
+mv master master.zip
+unzip master.zip && cd PyVCF-master && python setup.py install && cd .. && rm -Rf PyVCF-master && rm -f master.* && rm -f master*
 
 echo " "
 echo "BioPython  ========================="
 pip install biopython
+
+echo " "
+echo "Installing most dependencies that can be downloaded from pypi"
+
+echo "bcbio-gff ========================="
+pip install bcbio-gff
+echo " "
+echo "pysam ========================="
+pip install pysam
+echo " "
+echo "pandas ========================="
+pip install pandas
+echo " "
+echo "cython ========================="
+pip install cython
+echo " "
+echo "shove ========================="
+pip install shove
+echo " "
+echo "sqlalchemy ========================="
+pip install sqlalchemy
+echo " "
+echo "python-memcached ========================="
+pip install python-memcached
+
+echo " NOTE:  Some dependencies may not have been installed, you should still install oncotator normally."
 
 deactivate
