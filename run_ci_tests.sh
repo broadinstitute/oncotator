b327802e5cfbad4ecde2329d86b9b354c7449061
Lee Lichtenstein <lichtens@broadinstitute.org>
2013-12-18 09:17:46 -0500

    Fixed bug in reference datasource that could cause duplicate annotation exception .  Moving CLI tests before the unit tests themselves.

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -30,17 +30,23 @@ mkdir -p out
 # Create a proper config file
 sed -r "s:dbDir=MY_DB_DIR:dbDir=${DB_DIR}:g" test/configs/personal-test.config.template >test/configs/personal-test.config
 
+# Attempt simple command line functionality
+echo "Attempting a few command line calls (oncotator only) to make sure there are no egregious errors in the Oncotator CLI.
+echo "== Just running --help ==="
+oncotator --help
+
+echo "== Indel maflite 2 tcga maf test ==="
+oncotator -v --no-multicore --db-dir=${DB_DIR} test/testdata/maflite/Patient0.indel.maf.txt $VENV/test_Patient0.indel.maf.txt hg19
+
+echo "== SNV maflite 2 tcga maf test ==="
+oncotator -v --no-multicore --db-dir=${DB_DIR} test/testdata/maflite/Patient0.snp.maf.txt $VENV/test_Patient0.snp.maf.txt hg19
+########
+
 set +e
 # Do not use multiprocess mode with profiling or coverage.  Bug in nosetests also disallows --processes and --with-xunit
 #  --processes=4 --process-timeout=480  --process-restartworker
 nosetests --all-modules --exe --with-xunit --xunit-file=nosetests.xml -w test -v
 set -e
-
-# Attempt simple command line functionality
-echo "Attempting a few command line calls (oncotator only) to make sure there are no egregious errors in the Oncotator CLI.
-oncotator --help
-oncotator -v --no-multicore --db-dir=${DB_DIR} test/testdata/maflite/Patient0.indel.maf.txt $VENV/test_Patient0.indel.maf.txt hg19
-oncotator -v --no-multicore --db-dir=${DB_DIR} test/testdata/maflite/Patient0.snp.maf.txt $VENV/test_Patient0.snp.maf.txt hg19
 
 echo "Deactivating and deleting test python virtual environment"
 deactivate
