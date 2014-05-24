5f62d19030425e576af598fd16d57b67ec2f83ad
elephanhunter <manaswigupta@gmail.com>
2014-05-23 21:04:50 -0400

    dbNSFP changes. Indelocater fixes.

--- scripts/dbnsfp_utils/README.txt
+++ scripts/dbnsfp_utils/README.txt
@@ -1,5 +1,5 @@
 Please follow the steps below in order to create a dbNSFP data source:
 1. Download zipped database from https://sites.google.com/site/jpopgen/dbNSFP (example: http://dbnsfp.houstonbioinformatics.org/dbNSFPzip/dbNSFPv2.4.zip).
-2. Unzip using java
-3. Use script to concatenate
-4. Use initializeDatasource to build data source
+2. Unzip using java's jar command (example: jar xvf dbNSFPv2.4.zip)
+3. Use concatenatedbNSFPTsvs.py to concatenate the tsv files (example: python concatenatedbNSFPTsvs.py --dir ~/dbSNFP --prefix dbNSFP2.4_variant.chr --out ~/dbNSFP2.4_variant.tsv)
+4. Use initializeDatasource to build the data source (example: )
--- scripts/dbnsfp_utils/concatenatedbNSFPTsvs.py
+++ scripts/dbnsfp_utils/concatenatedbNSFPTsvs.py
@@ -1,26 +1,46 @@
 import argparse
 import os
+import csv
+import string
 
 
 def main():
     parser = argparse.ArgumentParser(description="Given a directory, this script creates a concatenated file that "
-                                                 "consists .")
+                                                 "consists . Consistent column ordering, commenting and header location are assumed.")
 
     parser.add_argument("--dir", action="store", type=str, dest="dir", required=True,
                         help="Directory where the tsv files are located")
     parser.add_argument("--prefix", action="store", type=str, dest="prefix", required=True,
                         help="Prefix for the tsv files")
-    parser.add_argument("--out", action="store", type=str, dest="out", required=True,
+    parser.add_argument("--out", action="store", type=str, dest="out",
                         help="Name of the concatenated output file")
 
     args, _ = parser.parse_known_args()
 
     # Collect the names of all tsv files with the prefix
+    filenames = [os.path.join(args.dir, entry) for entry in os.listdir(args.dir) if entry.startswith(args.prefix)]
+    filenames = [filename for filename in filenames if os.path.isfile(filename)]
 
+    filename = args.out if args.out is not None else string.join([args.prefix, ".tsv"], ".")
+    with open(filename, "w") as outfile:  # Iterate through files and concatenate them
+        writer = None
+        for filename in filenames:
+            with open(filename, "r") as infile:
+                reader = csv.DictReader(infile, delimiter="\t")
+                if writer is None:
+                    writer = csv.DictWriter(outfile, delimiter="\t", fieldnames=reader.fieldnames)
+                    writer.writeheader()
+                rowdicts = []
+                index = 1
+                for rowdict in reader:
+                    rowdicts += [rowdict]
+                    if index % 10000 == 0:
+                        writer.writerows(rowdicts)
+                        rowdicts = []
+                        index += 1
 
-    # Concatenate files
-
-
+                if index != 0:
+                    writer.writerows(rowdicts)
 
 if __name__ == "__main__":
     main()
