921e5d511e3594d39a4e100dc1699bcd242528e8
Lee Lichtenstein <lichtens@broadinstitute.org>
2014-06-18 20:59:21 -0400

    Minor changes that will still need cleanup.

--- oncotator/datasources/ShoveDatasource.py
+++ oncotator/datasources/ShoveDatasource.py
@@ -17,7 +17,7 @@ class ShoveDatasource(Datasource):
 
     """
 
-    def __init__(self, src_file, title, version, index_cols, shove_cache_url="simple://", max_entries=500):
+    def __init__(self, src_file, title, version, index_cols, annotation_columns, shove_cache_url="simple://", max_entries=500):
         """
 
         :param src_file: Shove URI for store
@@ -29,19 +29,39 @@ class ShoveDatasource(Datasource):
         """
         super(ShoveDatasource, self).__init__(src_file, title=title, version=version)
         self._db_store = Shove(src_file, shove_cache_url, max_entries=max_entries)
+        self._annotation_columns = annotation_columns
 
     def annotate_mutation(self, mutation):
         """ Mutations are annotated only with exact matches of chr, start, end, ref, and alt.
         """
 
         # create hash for this mutation
+        h = ShoveDatasource.generate_hash(mutation)
 
         # extract value for this hash from the db
+        annotations_list = []
+        try:
+            annotations_list = self._db_store[h]
+        except KeyError:
+            # do nothing
+            pass
 
         # Annotate
+        for i,col in enumerate(self._annotation_columns):
+            if len(annotations_list) <= i:
+                mutation.createAnnotation(col, "", self.title)
+            else:
+                mutation.createAnnotation(col, annotations_list[i], self.title)
+
+        return mutation
+
+
 
     @staticmethod
     def generate_hash(m):
-        return "%s_%s_%s_%s_%s" % (m.chr, m.start, m.end, m.ref_allele, m.alt_allele)
+        return ShoveDatasource.generate_hash(m.chr, m.start, m.end, m.ref_allele, m.alt_allele)
 
+    @staticmethod
+    def generate_hash(chrom, start,end,ref,alt):
+        return "%s_%s_%s_%s_%s" % (chrom, start, end, ref, alt)
 
