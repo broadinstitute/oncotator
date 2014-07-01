8ad3513dd13651fe63383977e4b414d8eb3b6655
Lee Lichtenstein <lichtens@broadinstitute.org>
2014-06-30 21:45:10 -0400

    Added some more messages re: GATK VCF

--- oncotator/utils/VariantClassifier.py
+++ oncotator/utils/VariantClassifier.py
@@ -327,6 +327,7 @@ class VariantClassifier(object):
                 logging.getLogger(__name__).error("Could not properly adjust protein position for variant: %s, %s, %s, %s, %s VT: %s" % (tx.get_contig(), start, end, ref_allele, alt_allele, variant_type))
                 logging.getLogger(__name__).error(str(ive))
                 logging.getLogger(__name__).warn("Above error may not have exact start and end positions if this is a VCF input.")
+                logging.getLogger(__name__).warn("Variant type is likely incorrect.  This can happen with some GATK VCFs")
                 logging.getLogger(__name__).warn("The protein_change annotation may not be properly rendered.")
 
         vc_tmp, vc_tmp_secondary = self.infer_variant_classification(variant_type, reference_aa, observed_aa, ref_allele, alt_allele,
