4b296696d50d8ecb0fba2e96134d67021f625270
Alex Ramos <alexisramos@gmail.com>
2015-05-12 14:58:39 -0400

    Added skipIfs for BigWig tests to check if ngslib is installed

--- test/BigWigTest.py
+++ test/BigWigTest.py
@@ -5,6 +5,14 @@ from oncotator.MutationData import MutationData
 import oncotator.datasources.BigWigDatasource
 from TestUtils import TestUtils
 
+try:
+    import ngslib
+except ImportError as e:
+    if 'ngslib' in e.message:
+        NGSLIB_INSTALLED = False
+else:
+    NGSLIB_INSTALLED = True
+
 TestUtils.setupLogging(__file__, __name__)
 
 class BigWigDatasourceTest(unittest.TestCase):
@@ -17,6 +25,7 @@ class BigWigDatasourceTest(unittest.TestCase):
     def tearDown(self):
         self.bigwig_datasource.close()
 
+    @unittest.skipIf(not NGSLIB_INSTALLED, 'ngslib not installed, skipping test')
     def test_basic_fetch(self):
         m = MutationData()
         m.createAnnotation('chr', '1')
@@ -26,6 +35,7 @@ class BigWigDatasourceTest(unittest.TestCase):
         self.bigwig_datasource.annotate_mutation(m)
         self.assertEqual(m.get('TestBigWig_score'), 0.5)
 
+    @unittest.skipIf(not NGSLIB_INSTALLED, 'ngslib not installed, skipping test')
     def test_range_fetch(self):
         m = MutationData()
         m.createAnnotation('chr', '1')
@@ -35,6 +45,7 @@ class BigWigDatasourceTest(unittest.TestCase):
         self.bigwig_datasource.annotate_mutation(m)
         self.assertEqual(m.get('TestBigWig_score'), 0.75)
 
+    @unittest.skipIf(not NGSLIB_INSTALLED, 'ngslib not installed, skipping test')
     def test_no_data_fetch(self):
         """Test for value not found in bigwig.  In this case, our test bigwig only has data for 
         chr1 so None is expected return value.
